b661f43d7d95cebb3b5c00a61b72e11ca7cb76ed
Lakshmi0523 <90890625+Lakshmi0523@users.noreply.github.com>
2024-07-08 17:26:39 +0530

    Add files via upload

--- config.php
+++ config.php
@@ -1,12 +1,17 @@
 <?php
 
-$dbhost = "starttooldev01.mysql.database.azure.com";
-$dbname = "startdb";
-$dbuser = "srtadmin01";
-$dbpass ="Ugw6h1daE5";
+//$dbhost = "starttooldev01.mysql.database.azure.com";
+//$dbname = "startdb";
+//$dbuser = "srtadmin01";
+//$dbpass ="Ugw6h1daE5";
+
+$host = getenv('dbhost');
+$name = getenv('dbname');
+$user = getenv('dbuser');
+$pass = getenv('dbpass');
 
 
-$link = @mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+$link = @mysqli_connect($host, $user, $pass, $name);
 if (mysqli_connect_errno()) {
 	 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	exit();
