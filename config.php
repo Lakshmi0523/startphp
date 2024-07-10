664f873fee1d2c446cc57c4ca8f45be862402426
Lakshmi0523 <90890625+Lakshmi0523@users.noreply.github.com>
2024-07-10 17:39:26 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $dbuser = "srtadmin01";
 $dbpass ="Ugw6h1daE5";
 echo"conect database";
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+//mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, 'starttooldev01.mysql.database.azure.com', 'srtadmin01', 'Ugw6h1daE5', 'startdb', 3306, MYSQLI_CLIENT_SSL);
 //$link = @mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 echo"Connection happing here";
