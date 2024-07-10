45421252a910e528f20dd3dcf499a885d962433f
Lakshmi0523 <90890625+Lakshmi0523@users.noreply.github.com>
2024-07-10 12:56:55 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,16 @@
 <?php
+$dbhost = "starttooldev01.mysql.database.azure.com";
+$dbname = "startdb";
+$dbuser = "srtadmin01";
+$dbpass ="Ugw6h1daE5";
 
-//$dbhost = "starttooldev01.mysql.database.azure.com";
-//$dbname = "startdb";
-//$dbuser = "srtadmin01";
-//$dbpass ="Ugw6h1daE5";
-
-$host = getenv('dbhost');
-$name = getenv('dbname');
-$user = getenv('dbuser');
-$pass = getenv('dbpass');
+//$host = getenv('dbhost');
+//$name = getenv('dbname');
+//$user = getenv('dbuser');
+//$pass = getenv('dbpass');
 
 
-$link = @mysqli_connect($host, $user, $pass, $name);
+$link = @mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if (mysqli_connect_errno()) {
 	 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	exit();
