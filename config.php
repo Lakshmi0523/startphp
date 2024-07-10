a1ffe70ad31d4cf6f56ce26652cbc1208876a622
Lakshmi0523 <90890625+Lakshmi0523@users.noreply.github.com>
2024-07-10 13:30:44 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,13 +4,13 @@ $dbname = "startdb";
 $dbuser = "srtadmin01";
 $dbpass ="Ugw6h1daE5";
 echo"conect database";
-//$conn = mysqli_init();
-//mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-//mysqli_real_connect($conn, 'starttooldev01.mysql.database.azure.com', 'srtadmin01', 'Ugw6h1daE5', 'startdb', 3306);
-$link = @mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, 'starttooldev01.mysql.database.azure.com', 'srtadmin01', 'Ugw6h1daE5', 'startdb', 3306);
+//$link = @mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if (mysqli_connect_errno()) {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
-//die('Failed to connect to MySQL: '.mysqli_connect_error());
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 exit();
 }
 
