694cf83e7f652c85ee4b08a386886a2edf65c522
Lakshmi0523 <90890625+Lakshmi0523@users.noreply.github.com>
2024-07-10 13:20:30 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,20 @@
 <?php
-$dbhost = "starttooldev01.mysql.database.azure.com";
-$dbname = "startdb";
-$dbuser = "srtadmin01";
-$dbpass ="Ugw6h1daE5";
+//$dbhost = "starttooldev01.mysql.database.azure.com";
+//$dbname = "startdb";
+//$dbuser = "srtadmin01";
+//$dbpass ="Ugw6h1daE5";
 
-//$host = getenv('dbhost');
-//$name = getenv('dbname');
-//$user = getenv('dbuser');
-//$pass = getenv('dbpass');
-
-
-$link = @mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, 'starttooldev01.mysql.database.azure.com', 'srtadmin01', 'Ugw6h1daE5', 'startdb', 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno()) {
-	 echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	exit();
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+
+
+//$link = @mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+//if (mysqli_connect_errno()) {
+	// echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	//exit();
+//}
 ?>
