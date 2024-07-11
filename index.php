e1ca6e6ba48f0254136a01d9c1b4e061d67e352a
Lakshmi0523 <90890625+Lakshmi0523@users.noreply.github.com>
2024-07-11 14:28:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ else
 $Sql_Query = "INSERT INTO start_hits (hit_date,user_name,version) values(now(),'".$arr[0]."','".$arr[1]."')";
        echo "$Sql_Query" ;
  try {
-             $result = mysqli_query($link,$Sql_Query);
+             $result = mysqli_query($conn,$Sql_Query);
              if (!$result) {
                  throw new Exception(mysqli_error($link));
              }
