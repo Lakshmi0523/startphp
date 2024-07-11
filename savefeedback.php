9509e3327af276a03976f75e4ed881a4f95ef4c0
Lakshmi0523 <90890625+Lakshmi0523@users.noreply.github.com>
2024-07-11 17:21:21 +0530

    Update savefeedback.php

--- savefeedback.php
+++ savefeedback.php
@@ -1,8 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
  include_once("config.php");
-
+echo "Feedback";
 if($_SERVER['REQUEST_METHOD']=='POST'){
 
 $encrypted=$_POST['d1']; 
